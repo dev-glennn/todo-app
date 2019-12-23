8ce95d78e53af3aa8a3ec38444c3b902f7fdba6e
dev-glennn <dev.glennn@gmail.com>
2019-12-23 10:25:26 +0900

    -
    -

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useState, useRef, useCallback, useReducer } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
@@ -15,41 +15,47 @@ function createBulkTodos() {
   return array;
 }
 
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT':
+      return todos.concat(action.todo);
+    case 'REMOVE':
+      return todos.filter(todo => todo.id !== action.id);
+    case 'TOGGLE':
+      return todos.map(todo =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
+    default:
+      return todos;
+  }
+}
+
 const App = () => {
-  // const [todos, setTodos] = useState([
-  //   { id: 1, text: '리액트의 기초 알아보기', checked: true },
-  //   { id: 2, text: '컴포넌트 스타일링 해보기', checked: true },
-  //   { id: 3, text: '일정 관리 앱 만들어 보기', checked: false },
-  // ]);
-  const [todos, setTodos] = useState(createBulkTodos);
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
+  // const [todos, setTodos] = useState(createBulkTodos);
   const nextId = useRef(2501);
 
-  const onInsert = useCallback(
-    text => {
-      const todo = { id: nextId.current, text, checked: false };
-      setTodos(todos.concat(todo));
-      nextId.current += 1;
-    },
-    [todos],
-  );
+  const onInsert = useCallback(text => {
+    const todo = { id: nextId.current, text, checked: false };
+    // setTodos(todos.concat(todo));
+    // setTodos(todos => todos.concat(todo));
+    dispatch({ type: 'INSERT', todo });
+    nextId.current += 1;
+  }, []);
 
-  const onRemove = useCallback(
-    id => {
-      setTodos(todos.filter(todo => todo.id !== id));
-    },
-    [todos],
-  );
+  const onRemove = useCallback(id => {
+    // setTodos(todos => todos.filter(todo => todo.id !== id));
+    dispatch({ type: 'REMOVE', id });
+  }, []);
 
-  const onToggle = useCallback(
-    id => {
-      setTodos(
-        todos.map(todo =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
-    },
-    [todos],
-  );
+  const onToggle = useCallback(id => {
+    // setTodos(todos =>
+    // todos.map(todo =>
+    // todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+    // ),
+    // );
+    dispatch({ type: 'TOGGLE', id });
+  }, []);
 
   return (
     <div>
@@ -62,3 +68,9 @@ const App = () => {
 };
 
 export default App;
+
+// const [todos, setTodos] = useState([
+//   { id: 1, text: '리액트의 기초 알아보기', checked: true },
+//   { id: 2, text: '컴포넌트 스타일링 해보기', checked: true },
+//   { id: 3, text: '일정 관리 앱 만들어 보기', checked: false },
+// ]);
